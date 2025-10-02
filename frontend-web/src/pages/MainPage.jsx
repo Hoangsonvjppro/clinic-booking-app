9ff03789559412499063861f28bb706bf1c8d3ce
Shovul <140464903+Shovul@users.noreply.github.com>
2025-10-02 13:44:52 +0700

    Setup framework cho frontend

---
+++ frontend-web/src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div className='flex flex-col justify-center items-center text-2xl'>
+            <h1>Welcome to Clinic Booking App</h1>
+            <p>This is the main page. Start booking your appointments!</p>
+        </div>
+    );
+};
+
+export default MainPage;
