86656c5d74ae043ff1658b06cacbe0a09f2b4917
Hoangsonvjppro <sonleanit8386@gmail.com>
2025-10-23 14:59:50 +0700

    add CrossOrigin in to all controller

--- auth-service/src/main/java/com/clinic/auth/web/RoleController.java
+++ auth-service/src/main/java/com/clinic/auth/web/RoleController.java
@@ -10,6 +10,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Locale;
 import java.util.Set;
 
+@CrossOrigin("https://hoofed-alfonzo-conclusional.ngrok-free.dev")
 @RestController
 @RequestMapping("/api/v1/roles")
 @RequiredArgsConstructor
