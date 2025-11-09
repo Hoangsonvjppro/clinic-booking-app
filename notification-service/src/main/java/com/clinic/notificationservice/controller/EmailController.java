54d85b644ae59f8041cdf76202b00348de4694bd
tmdkhac12 <quannguyen081204@gmail.com>
2025-11-09 17:42:56 +0700

    Add Send Email Template

--- notification-service/src/main/java/com/clinic/notificationservice/controller/EmailController.java
+++ notification-service/src/main/java/com/clinic/notificationservice/controller/EmailController.java
@@ -4,6 +4,8 @@ import com.clinic.notificationservice.services.EmailService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
 public class EmailController implements CommandLineRunner {
 
@@ -13,11 +15,26 @@ public class EmailController implements CommandLineRunner {
         this.emailService = emailService;
     }
 
+//    @Override
+//    public void run(String... args) throws Exception {
+//        String toEmail = "tuyetan419@gmail.com";
+//        String subject = "Test notification service";
+//        String body = "Hello world";
+//        emailService.sendEmail(toEmail, subject, body);
+//    }
+
     @Override
     public void run(String... args) throws Exception {
         String toEmail = "tuyetan419@gmail.com";
-        String subject = "Test notification service";
-        String body = "Hello world";
-        emailService.sendEmail(toEmail, subject, body);
+        String subject = "Xác nhận lịch hẹn khám chữa bệnh";
+        Map<String, Object> body = Map.of(
+                "patientName", "Nguyễn Khắc Đông Quân",
+                "appointmentTime", "12/11/2025 09:00:00",
+                "doctorName", "Võ Cao Sang",
+                "notes", "Kiểm tra tình trạng sức khỏe",
+                "status", "Đã xác nhận"
+        );
+        emailService.sendEmailHtml(toEmail, subject, body);
+        System.out.println("Gui mail thanh cong");
     }
 }
--- notification-service/src/main/java/com/clinic/notificationservice/services/EmailService.java
+++ notification-service/src/main/java/com/clinic/notificationservice/services/EmailService.java
@@ -8,15 +8,24 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.util.Map;
 
 @Service
 public class EmailService {
 
-    @Autowired
     private JavaMailSender mailSender;
+    private SpringTemplateEngine templateEngine;
 
     @Value("${spring.mail.username}")
     private String fromEmail;
+
+    public EmailService(JavaMailSender mailSender, SpringTemplateEngine templateEngine) {
+        this.mailSender = mailSender;
+        this.templateEngine = templateEngine;
+    }
 
     public void sendEmail(String toEmail, String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
@@ -30,16 +39,22 @@ public class EmailService {
         System.out.println("Gui mail thanh cong");
     }
 
-    public void sendEmailHtml(String toEmail, String subject, String htmlContent) throws MessagingException {
+    public void sendEmailHtml(String toEmail, String subject, Map<String, Object> variables) throws MessagingException {
+        // Prepare thymeleaf context
+        Context context = new Context();
+        context.setVariables(variables);
+
+        String htmlContent = templateEngine.process("template-email", context);
+
+        // Prepare mime message
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
-        helper.setFrom("");
+        helper.setFrom(fromEmail);
         helper.setTo(toEmail);
         helper.setSubject(subject);
         helper.setText(htmlContent, true);
 
         mailSender.send(message);
-        System.out.println("Gui mail thanh cong");
     }
 }
